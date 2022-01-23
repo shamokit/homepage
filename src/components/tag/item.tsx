c040769643c8396e41f0f310aaeaac185beccb21
shamokit <contact@shamokit.com>
2022-01-24 01:14:05 +0900

    スタイル変更

--- src/components/tag/item.tsx
+++ src/components/tag/item.tsx
@@ -15,7 +15,7 @@ const TagItem = ({
   if (link) {
     return (
       <Link href={`/${dirName}/tag/${slug}/`}>
-        <a className="flex place-items-center px-3 py-2 font-code rounded bg-base-color text-white leading-tight transition-colors hover:bg-main">
+        <a className="flex place-items-center px-3 py-2 font-code rounded bg-base-color text-xs text-white leading-tight transition-colors hover:bg-main">
           {name}
         </a>
       </Link>
