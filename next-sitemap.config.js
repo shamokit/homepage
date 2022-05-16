d5bf2d085e253f056bd8a9dc31a1e12b0395dcac
shamokit <contact@shamokit.com>
2022-05-17 01:01:28 +0900

    不要なので除外

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://shamokit.com',
+	exclude: ['/diaries/*', '/books/*'],
   generateRobotsTxt: true,
 }
