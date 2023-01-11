33273dc5e4ea871af00f6dbdb3e3d8644e9220c8
shamokit <contact@shamokit.com>
2023-01-11 21:14:04 +0900

    feat: QiitaとZennの記事を取得

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,6 +2,7 @@ import { error } from '@sveltejs/kit';
 import { env } from '$env/dynamic/private';
 import type { PageServerLoad } from './$types';
 import axios from 'axios';
+import Parser from "rss-parser"
 export const load = (async () => {
 	const { INSTAGRAM_BUSINESS_ACCOUNT_ID, INSTAGRAM_ACCESS_TOKEN } = env;
 	const instagramUrl = `https://graph.facebook.com/v15.0/${INSTAGRAM_BUSINESS_ACCOUNT_ID}?fields=name,media.limit(12){media_url,permalink}&access_token=${INSTAGRAM_ACCESS_TOKEN}`;
@@ -14,8 +15,22 @@ export const load = (async () => {
 			}[];
 		};
 	}>(instagramUrl);
-	if (photos) {
-		return photos.data.media;
+
+	const parser = new Parser<{
+		id: string,
+		link: string,
+		title: string,
+		isoDate: string,
+	}>();
+	const qiita = await parser.parseURL('https://qiita.com/shamokit/feed.atom');
+	const zenn = await parser.parseURL('https://zenn.dev/shamokit/feed');
+	const postNum = 4;
+	if (photos && qiita && zenn) {
+		return {
+			photos: photos.data.media,
+			zenn: zenn.items.slice(0, postNum),
+			qiita: qiita.items.slice(0, postNum),
+		};
 	}
-	throw error(400, '写真を取得できませんでした。');
+	throw error(400, 'エラー');
 }) satisfies PageServerLoad;
