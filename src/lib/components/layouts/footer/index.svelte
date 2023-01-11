805f3287f7d7e8115f18d45dd776f6f600dc9386
shamokit <contact@shamokit.com>
2023-01-11 21:12:18 +0900

    fix: フッター調整

--- src/lib/components/layouts/footer/index.svelte
+++ src/lib/components/layouts/footer/index.svelte
@@ -11,40 +11,40 @@
 <footer class="bg-surface-700">
 	<div class="container py-6 lg:py-16">
 		<div class="flex flex-col gap-3 lg:flex-row lg:items-end">
-			<ul class="flex gap-x-3 lg:gap-x-5 lg:order-1">
+			<ul class="flex gap-x-3 lg:order-1 -mx-1">
 				<li>
-					<a href={contactList.qiita.url} target="_blank" rel="noopener noreferrer" class="block w-6 lg:w-10 transition-opacity hover:opacity-80"
+					<a href={contactList.qiita.url} target="_blank" rel="noopener noreferrer" class="block w-7 p-1 lg:w-12 rounded-full transition-opacity hover:opacity-80"
 						><img src={qiita} alt={contactList.qiita.name} width="40" height="40" /></a
 					>
 				</li>
 				<li>
-					<a href={contactList.zenn.url} target="_blank" rel="noopener noreferrer" class="block w-6 lg:w-10 transition-opacity hover:opacity-80"
+					<a href={contactList.zenn.url} target="_blank" rel="noopener noreferrer" class="block w-7 p-1 lg:w-12 rounded-full transition-opacity hover:opacity-80"
 						><img src={zenn} alt={contactList.zenn.name} width="40" height="40" /></a
 					>
 				</li>
 				<li>
-					<a href={contactList.instagram.url} target="_blank" rel="noopener noreferrer" class="block w-6 lg:w-10 transition-opacity hover:opacity-80"
+					<a href={contactList.instagram.url} target="_blank" rel="noopener noreferrer" class="block w-7 p-1 lg:w-12 rounded-full transition-opacity hover:opacity-80"
 						><img src={instagram} alt={contactList.instagram.name} width="40" height="40" /></a
 					>
 				</li>
 				<li>
-					<a href={contactList.twitter.url} target="_blank" rel="noopener noreferrer" class="block w-6 lg:w-10 transition-opacity hover:opacity-80"
+					<a href={contactList.twitter.url} target="_blank" rel="noopener noreferrer" class="block w-7 p-1 lg:w-12 rounded-full transition-opacity hover:opacity-80"
 						><img src={twitter} alt={contactList.twitter.name} width="40" height="40" /></a
 					>
 				</li>
 				<li>
-					<a href={contactList.github.url} target="_blank" rel="noopener noreferrer" class="block w-6 lg:w-10 transition-opacity hover:opacity-80"
+					<a href={contactList.github.url} target="_blank" rel="noopener noreferrer" class="block w-7 p-1 lg:w-12 rounded-full transition-opacity hover:opacity-80"
 						><img src={github} alt={contactList.github.name} width="40" height="40" /></a
 					>
 				</li>
 			</ul>
 			<div class="grid gap-2 flex-1">
-				<ul class="grid w-fit grid-cols-2 lg:flex gap-y-3 gap-x-5">
+				<ul class="flex flex-wrap md:gap-y-3 -mx-2.5">
 					{#each links as link}
-					<li>
+					<li class="lg:py-1">
 						<a
 							href={link.slug}
-							class="flex items-center gap-1.5 transition-all hover:text-secondary-500 hover:underline-offset-8 hover:underline"
+							class="flex items-center gap-1.5 px-2.5 md:py-2 transition-all rounded-full hover:text-secondary-500 hover:underline-offset-8 hover:underline"
 						>
 							{#if link.iconName}
 								<span class="inline-block w-5"><Ico name={link.iconName} /></span>
