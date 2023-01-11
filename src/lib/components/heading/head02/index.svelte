b62cc66b99b520351702aef4c9b40fe9d2ae0e9a
shamokit <contact@shamokit.com>
2023-01-11 21:12:08 +0900

    feat: 見出し2追加

---
+++ src/lib/components/heading/head02/index.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let title: string = ''
+	export let headingLevel: 3 | 4 = 3
+	$: tagName = `h${headingLevel}`
+</script>
+<header class="flex flex-wrap items-center gap-5">
+	<div class="flex items-center gap-3">
+		{#if $$slots.ico}
+			<div class="w-6 h-6 lg:w-10 lg:h-10">
+				<slot name="ico" />
+			</div>
+		{/if}
+		<svelte:element this={tagName} class="text-xl">
+			{title}
+		</svelte:element>
+	</div>
+	{#if $$slots.button}
+		<slot name="button" />
+	{/if}
+</header>
