bf85e385a40dfa2fbed7d51b4835d7d3fe2066c3
shamokit <contact@shamokit.com>
2023-01-11 21:11:56 +0900

    fix: ホバー時のスタイル

--- src/lib/components/button/linkButton/index.svelte
+++ src/lib/components/button/linkButton/index.svelte
@@ -4,8 +4,8 @@
 	export let blank: boolean = false
 </script>
 <p class="text-sm">
-	<a href={href} target={blank ? "_blank" : null} rel={blank ? "noopener noreferrer" : null} class="flex items-center gap-2 w-fit pt-[13px] pb-3 px-6 bg-primary-500 text-surface-100 rounded-full">
+	<a href={href} target={blank ? "_blank" : null} rel={blank ? "noopener noreferrer" : null} class="flex items-center gap-2 w-fit pt-[13px] pb-3 px-6 bg-primary-500 border-1 border-primary-500 text-surface-100 font-bold rounded-full transition-colors will-change-colors duration-300 ease-in-out hover:shadow-sm hover:bg-surface-100 hover:text-primary-100">
 		<slot />
-		<span class="inline-flex w-4 flex-shrink-0 flex-grow-0" aria-label="外部サイトに移動します"><Ico name="blank" /></span>
+		<span class="inline-flex w-4 flex-shrink-0 flex-grow-0"><Ico name="blank" /><span class="sr-only">外部サイトに移動します</span></span>
 	</a>
 </p>
