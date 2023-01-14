6c299cf8ef006d441d09f7adf0ea37871d7a28d0
shamokit <contact@shamokit.com>
2023-01-14 23:59:35 +0900

    feat: エラーページ

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { page } from '$app/stores';
+	import Head01 from '$lib/components/heading/head01/index.svelte';
+</script>
+<svelte:head>
+	<title>Error！{$page.status}</title>
+</svelte:head>
+<section>
+	<div class="grid gap-12 container py-16 lg:py-28">
+		<Head01 title="Error">
+			<p>Status: {$page.status}</p>
+		</Head01>
+		<p>{$page.error?.message}</p>
+	</div>
+</section>
