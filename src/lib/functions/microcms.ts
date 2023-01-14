7f1a7c81db6a0f0a654dd9059a23db6537649425
shamokit <contact@shamokit.com>
2023-01-14 23:57:43 +0900

    fix: 移動して方もexport

--- src/lib/functions/microcms.ts
+++
@@ -1,6 +0,0 @@
-import { env } from '$env/dynamic/private';
-import { createClient } from 'microcms-js-sdk'
-export const client = createClient({
-  serviceDomain: "shamokit",
-  apiKey: env.CMS_API_KEY,
-});
