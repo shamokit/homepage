adcb906b3f730667202ca5dd93f7fbf384491d85
shamokit <contact@shamokit.com>
2023-01-14 23:58:29 +0900

    feat:日付コンポーネント

---
+++ src/lib/components/date/dates.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import dayjs from 'dayjs';
+	dayjs.locale('ja');
+	export let publishedDate: string = '';
+	export let updatedDate: string = '';
+	import Date from '$lib/components/date/index.svelte';
+</script>
+
+<ul class="flex gap-3 text-xxxs">
+	<Date date={publishedDate} type="edit" />
+	<Date date={updatedDate} type="updated" timeTag={true} />
+</ul>
