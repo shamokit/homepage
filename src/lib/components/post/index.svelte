c3ad486769b74d4d878d3032b0f22945d61c091f
shamokit <contact@shamokit.com>
2023-01-14 23:59:27 +0900

    feat: 記事表示コンポーネント

--- src/lib/components/post/index.svelte
+++ src/lib/components/post/index.svelte
@@ -1,24 +1,27 @@
 <script lang="ts">
-	import dayjs from 'dayjs'
-	dayjs.locale('ja')
 	import LogoIco from '$lib/components/logo/ico.svelte'
 	import Ico from '$lib/components/svg/ico.svelte'
-	export let url: string = ''
-	export let date: string = ''
-	export let title: string = ''
-	$: blank = !url.startsWith('https://shamokit.com')
-	$: formattedDate = dayjs(date).format('YYYY/MM/DD')
+	import Date from '$lib/components/date/index.svelte'
+	export let post: {
+		id: string,
+		link: string,
+		title: string,
+		isoDate: string,
+	}
+	$: blank = !post.link.startsWith('https://shamokit.com')
 </script>
-<a href={url} class="group relative flex gap-3 p-4 lg:p-6 bg-surface-100 visited:bg-surface-300 rounded-lg shadow-sm will-change-all transition-[box-shadow,background] duration-300 ease-in-out hover:bg-surface-100 hover:shadow-md"  target={blank ? "_blank" : null} rel={blank ? "noopener noreferrer" : null}>
+<a href={post.link} class="group relative flex gap-3 p-4 lg:p-6 bg-surface-100 visited:bg-surface-300 rounded-lg shadow-sm will-change-all transition-[box-shadow,background] duration-300 ease-in-out hover:bg-surface-100 hover:shadow-md"  target={blank ? "_blank" : null} rel={blank ? "noopener noreferrer" : null}>
 	<div class="w-9 flex-shrink-0">
 		<LogoIco />
 	</div>
 	<div class="grid gap-2 flex-1">
 		<div class="grid gap-1">
 			<p class="font-bold text-xs">@shamokit</p>
-			<p class="font-bold text-xxxs">{formattedDate}</p>
+			<div class="font-bold text-xxxs">
+				<Date date={post.isoDate} tag="p" hasIco={false} />
+			</div>
 		</div>
-		<p class="text-sm font-bold group-hover:text-secondary-500">{title}</p>
+		<p class="text-sm font-bold group-hover:text-secondary-500">{post.title}</p>
 	</div>
 	{#if blank}
 		<span class="absolute bottom-1 right-1 block w-3">
---
+++ src/lib/components/post/posts.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let posts: {
+		id: string,
+		link: string,
+		title: string,
+		isoDate: string,
+	}[]
+	import Post from '$lib/components/post/index.svelte';
+</script>
+<ul class="grid gap-3">
+	{#each posts as post}
+		<li>
+			<Post post={post} />
+		</li>
+	{/each}
+</ul>
