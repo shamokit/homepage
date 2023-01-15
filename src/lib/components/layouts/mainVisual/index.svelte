210827bcc7afd4746e9e69198382eeababa1d250
shamokit <contact@shamokit.com>
2023-01-16 01:07:45 +0900

    fix: フォントサイズを大きく

--- src/lib/components/layouts/mainVisual/index.svelte
+++ src/lib/components/layouts/mainVisual/index.svelte
@@ -4,7 +4,7 @@
 </script>
 <header>
 	<div class="grid gap-2.5 container">
-		<h1 class="text-xl">{title}</h1>
+		<h1 class="text-xxxl">{title}</h1>
 		{#if lead}
 			<p>{@html lead.replace(/\r?\n/g, '<br />')}</p>
 		{/if}
