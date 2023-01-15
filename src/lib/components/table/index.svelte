82d603dd58835f2231c9abdd6f2b620b05fbffe5
shamokit <contact@shamokit.com>
2023-01-16 01:07:59 +0900

    feat:テーブルコンポーネント

---
+++ src/lib/components/table/index.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Row from '$lib/components/table/row.svelte';
+	export let rows: {
+		title: string
+		body: string
+	}[]
+</script>
+<table class="bg-surface-100">
+	<tbody>
+		{#each rows as row}
+			<Row title={row.title} body={row.body} />
+		{/each}
+	</tbody>
+</table>
