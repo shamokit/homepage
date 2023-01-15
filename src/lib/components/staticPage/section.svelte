307a572bae74f8ccd9344c14d5defbbe148146a6
shamokit <contact@shamokit.com>
2023-01-16 01:10:18 +0900

    feat:staticPageコーディング

---
+++ src/lib/components/staticPage/section.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import type { Section } from '$lib/schema/staticPage/section';
+	import Toggle from '$lib/components/toggle/index.svelte';
+	import SectionContent from '$lib/components/staticPage/sectionContent.svelte';
+	export let section: Section
+	export let level: number = 2
+	$: headingTag = headingData({level, field: 'tag'})
+	$: headingClass = headingData({level, field: 'class'})
+	$: sectionTag = sectionData({level, field: 'tag'})
+	$: sectionClass = sectionData({level, field: 'class'})
+	const headingData = ({level, field}: {level: number, field: 'class' | 'tag'}) => {
+		switch(level) {
+			case 2: {
+				if(field === 'tag') return 'h2'
+				if(field === 'class') return 'text-xl'
+			}
+			case 3: {
+				if(field === 'tag') return 'h3'
+				if(field === 'class') return 'text-lg'
+			}
+			default: {
+				if(field === 'tag') return 'h4'
+				if(field === 'class') return 'text-md'
+			}
+		}
+	}
+	const sectionData = ({level, field}: {level: number, field: 'class' | 'tag'}) => {
+		const sectionTag = section.toggle && section.title ? 'div' : 'section'
+		switch(level) {
+			case 2: {
+				if(field === 'tag') return sectionTag
+				if(field === 'class') return 'gap-5 py-7'
+			}
+			case 3: {
+				if(field === 'tag') return sectionTag
+				if(field === 'class') return 'gap-4 py-6 px-7 bg-surface-500'
+			}
+			default: {
+				if(field === 'tag') return sectionTag
+				if(field === 'class') return 'gap-3 py-3'
+			}
+		}
+	}
+</script>
+{#if section.toggle && section.title}
+	<Toggle title={section.title}>
+		<SectionContent body={section.body} />
+	</Toggle>
+{:else}
+	<svelte:element this={sectionTag} class={`grid ${sectionClass}`}>
+		{#if section.title}
+			<svelte:element this={headingTag} class={headingClass}>{section.title}</svelte:element>
+		{/if}
+		<SectionContent body={section.body} level={level} />
+	</svelte:element>
+{/if}
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,90 +1,27 @@
 <script lang="ts">
 	import Breadcrumbs from '$lib/components/layouts/breadcrumbs/index.svelte'
-	import Ico from '$lib/components/svg/ico.svelte';
 	import MailVisual from '$lib/components/layouts/mainVisual/index.svelte';
+	import Section from '$lib/components/staticPage/section.svelte';
 	import type { PageData } from './$types';
 	export let data: PageData;
 </script>
 
 <svelte:head>
-	<title>{data.page.title}｜しゃもきっとブログ</title>
-	<meta name="description" content="しゃもきっとのエンジニアブログ" />
+	<title>{data.page.meta?.title ?? `${data.page.title}｜しゃもきっとブログ`}</title>
+	<meta name="description" content={data.page.meta?.description ?? `${data.page.title}ページ｜しゃもきっとブログ`} />
 </svelte:head>
 <Breadcrumbs breadcrumbs={[{
 	name: data.page.title,
 	slug: `/${data.page.slug}`
 }]} />
 <article>
-	<div class="h-28" />
+	<div class="h-8 md:h-14 lg:h-28" />
 	<MailVisual title={data.page.title} lead={data.page.lead.lead} />
-	<div class="h-16" />
+	<div class="h-4 md:h-8 lg:h-16" />
 	<div class="container">
 		{#each data.page.body.body as section}
-			<section class="grid gap-5 pt-7 pb-8">
-				{#if section.title}
-					<h2 class="text-xl">{section.title}</h2>
-				{/if}
-				<div class="grid gap-[1.8em]">
-					{#each section.body as childSection}
-						{#if childSection.fieldId === 'table'}
-							<table class="bg-surface-100">
-								<tbody>
-									{#each childSection.row as row}
-										<tr class="border-b border-primary-100">
-											<th class="py-3 px-6 text-left">{row.title}</th>
-											<td class="w-full py-3 px-6">{@html row.body}</td>
-										</tr>
-									{/each}
-								</tbody>
-							</table>
-						{/if}
-						{#if childSection.fieldId === 'content' && childSection.content}
-							<div class="grid gap-[1.8em]">{@html childSection.content.replace(/\r?\n/g, '<br />')}</div>
-						{/if}
-						{#if childSection.fieldId === 'section' && childSection.toggle}
-							<details class="bg-surface-100 shadow-sm rounded-md overflow-hidden">
-								<summary
-									class="group flex items-center gap-5 py-3 px-5 bg-primary-500 text-surface-100 font-bold"
-								>
-									<span class="mr-auto">{childSection.title}</span>
-									<span
-										class="block w-11 h-11 flex-shrink-0"
-										>
-										<span class="hidden group-open:block"><Ico name="minus" /></span>
-										<span class="block group-open:hidden"><Ico name="add" /></span>
-									</span>
-								</summary>
-								<div class="py-4 px-6">
-									{#if childSection.fieldId === 'section'}
-										{#each childSection.body as childSectionBody}
-											{#if childSectionBody.fieldId === 'histories'}
-												<ul>
-													{#each childSectionBody.history as history}
-														<li
-															class="py-4 border-b border-primary-500 last-of-type:border-0 align-top"
-														>
-															<div class="flex items-center">
-																<div class="mr-2 lg:mr-4"
-																	><span
-																		class="block w-5 h-5 lg:w-7 lg:h-7 border border-secondary-500 rounded-full"
-																	/></div
-																>
-																<p class="text-sm font-normal text-left">{history.day}</p>
-															</div>
-															<p class="mt-2 lg:mt-4 text-sm">{@html history.content}</p>
-														</li>
-													{/each}
-												</ul>
-											{/if}
-										{/each}
-									{/if}
-								</div>
-							</details>
-						{/if}
-					{/each}
-				</div>
-			</section>
+			<Section section={section} />
 		{/each}
 	</div>
-	<div class="h-28" />
+	<div class="h-8 md:h-14 lg:h-28" />
 </article>
