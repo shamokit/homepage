0dcb79844163d565107f128fbeee4aecd5446712
shamokit <contact@shamokit.com>
2023-01-16 01:10:46 +0900

    fix:thinking取得

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,6 +3,8 @@ import { env } from '$env/dynamic/private';
 import type { PageServerLoad } from './$types';
 import axios from 'axios';
 import Parser from "rss-parser"
+import { client } from '$lib/libs/microcms';
+
 export const load = (async () => {
 	const { INSTAGRAM_BUSINESS_ACCOUNT_ID, INSTAGRAM_ACCESS_TOKEN } = env;
 	const instagramUrl = `https://graph.facebook.com/v15.0/${INSTAGRAM_BUSINESS_ACCOUNT_ID}?fields=name,media.limit(12){media_url,permalink}&access_token=${INSTAGRAM_ACCESS_TOKEN}`;
@@ -27,8 +29,16 @@ export const load = (async () => {
 	const qiita = await parser.parseURL('https://qiita.com/shamokit/feed.atom');
 	const zenn = await parser.parseURL('https://zenn.dev/shamokit/feed');
 	const postNum = 4;
+
+	const thinkings = await client.getList({
+		endpoint: 'thinking',
+		queries: {
+			limit: 9
+		}
+	})
 	if (photos && qiita && zenn) {
 		return {
+			thinkings: thinkings,
 			photos: photos.data.media,
 			zenn: zenn.items.slice(0, postNum),
 			qiita: qiita.items.slice(0, postNum),
