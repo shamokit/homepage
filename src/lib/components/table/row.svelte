2f8f7503bc3808e022e4a3a08a4550469b0cdcfb
shamokit <contact@shamokit.com>
2023-01-17 00:43:49 +0900

    fix:row追加

--- src/lib/components/table/row.svelte
+++ src/lib/components/table/row.svelte
@@ -4,7 +4,7 @@
 	export let body: string
 </script>
 <tr class="border-b border-primary-100">
-	<th class="py-3 px-6 text-left">{title}</th>
+	<th class="py-3 px-6 text-left" scope="row">{title}</th>
 	<td class="w-full py-3 px-6">
 		<Wysiwyg content={body} />
 	</td>
