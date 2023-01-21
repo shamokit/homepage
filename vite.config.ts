b6dc786c6aaa7fb74b6044447f7adbb9cc9d9d02
shamokit <contact@shamokit.com>
2023-01-21 15:05:13 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import wasm from "vite-plugin-wasm";
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 const config: UserConfig = {
-	plugins: [wasm(), sveltekit()]
+	plugins: [wasm(), topLevelAwait(), sveltekit()],
+	worker: {
+		format: 'es',
+		plugins: [wasm(), topLevelAwait()]
+	}
 };
 export default config;
