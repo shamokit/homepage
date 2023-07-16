5174459b2e6d595f1238654a025f8bbca7a77756
shamokit <contact@shamokit.com>
2023-07-16 22:51:22 +0900

    ♻️ window.innerWidthからmediaQuery判定に修正

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,9 @@
 	import Svg from '$lib/components/svg/index.svelte'
 	import Header from '$lib/components/layouts/header/index.svelte'
 	import Footer from '$lib/components/layouts/footer/index.svelte'
+	import { mediaQueryAction } from '$lib/functions/mediaQuery.store.action'
 </script>
+<svelte:document use:mediaQueryAction />
 <Svg />
 <div class="flex flex-col min-h-screen pt-[var(--header-height)] bg-surface-300">
 	<Header isHome={$page.route.id === '/'} />
