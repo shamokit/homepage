1e93f75b26453f83d8dd066b967feeb001e47e92
shamokit <contact@shamokit.com>
2023-07-16 23:06:15 +0900

    ♻️ 読み上げの改善

--- src/lib/components/date/index.svelte
+++ src/lib/components/date/index.svelte
@@ -12,7 +12,7 @@
 
 <svelte:element this={tag} class="flex items-center gap-1">
 	{#if hasIco}
-		<span class="block flex-shrink-0 w-3"><Ico name={type} /></span>
+		<span class="block flex-shrink-0 w-3"><Ico name={type} ariaLabel={type === 'edit' ? '編集日：' : '更新日：'} /></span>
 	{/if}
 	<svelte:element this={timeTag ? 'time' : 'span'} datetime={timeTag ? date : null}>{formattedDate}</svelte:element>
 </svelte:element>
--- src/lib/components/svg/ico.svelte
+++ src/lib/components/svg/ico.svelte
@@ -2,8 +2,9 @@
 	import { icons, iconNames } from './const'
 	import { capitalize } from '$lib/functions/capitalize'
 	export let name: typeof iconNames[number] = 'edit'
+	export let ariaLabel: string | undefined = undefined
 	$: icon = icons[name]
 </script>
-<svg viewBox={icon.viewBox} class="fill-current">
+<svg viewBox={icon.viewBox} class="fill-current" aria-hidden={!ariaLabel && 'true'} aria-label={ariaLabel && ariaLabel} role={ariaLabel && 'img'}>
 	<use href={`#ico${capitalize(name)}`} />
 </svg>
