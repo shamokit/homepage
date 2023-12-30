5e673d66489f77a7f383f9fdd1b1eace7b07c527
shamokit <contact@shamokit.com>
2023-12-31 01:30:11 +0900

    🐛 layoutを修正

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
+	import { PropsWithChildren } from 'svelte';
 	import '../css/app.css';
 	import { page } from '$app/stores';
 	import Svg from '$lib/components/svg/index.svelte';
 	import Header from '$lib/components/layouts/header/index.svelte';
 	import Footer from '$lib/components/layouts/footer/index.svelte';
 	import { mediaQueryAction } from '$lib/functions/mediaQuery.store.action';
+
+	let { children } = $props<
+		PropsWithChildren<{
+			children: string;
+		}, never>
+	>();
 </script>
 
 <svelte:document use:mediaQueryAction />
@@ -12,7 +19,7 @@
 <div class="flex flex-col min-h-screen pt-[var(--header-height)] bg-surface-300">
 	<Header isHome={$page.route.id === '/'} />
 	<div class="flex-1">
-		<slot />
+		{@render children()}
 	</div>
 	<Footer />
 </div>
