cb038752c392d4d8702fa43066990829ac0b305a
shamokit <contact@shamokit.com>
2023-12-31 13:43:37 +0900

    mediaQueryActionを復活させてみる

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 	import Svg from '$lib/components/svg/index.svelte';
 	import Header from '$lib/components/layouts/header/index.svelte';
 	import Footer from '$lib/components/layouts/footer/index.svelte';
+	import { mediaQueryAction } from '$lib/functions/mediaQuery.store.action';
 
 	let { children } = $props<
 		PropsWithChildren<{
@@ -13,6 +14,7 @@
 	>();
 </script>
 
+<svelte:document use:mediaQueryAction />
 <div class="flex flex-col min-h-screen pt-[var(--header-height)] bg-surface-300 first:hidden">
 	<Svg />
 	<Header isHome={$page.route.id === '/'} />
