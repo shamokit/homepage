32c02949680b27708d21c683bc0619f30b395f13
shamokit <contact@shamokit.com>
2023-12-31 13:32:57 +0900

    storeまわりを消してみる

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,7 @@
 	import '../css/app.css';
 	import { page } from '$app/stores';
 	import Svg from '$lib/components/svg/index.svelte';
-	import Header from '$lib/components/layouts/header/index.svelte';
 	import Footer from '$lib/components/layouts/footer/index.svelte';
-	import { mediaQueryAction } from '$lib/functions/mediaQuery.store.action';
 
 	let { children } = $props<
 		PropsWithChildren<{
@@ -14,10 +12,8 @@
 	>();
 </script>
 
-<svelte:document use:mediaQueryAction />
 <div class="flex flex-col min-h-screen pt-[var(--header-height)] bg-surface-300 first:hidden">
 	<Svg />
-	<Header isHome={$page.route.id === '/'} />
 	<div class="flex-1">
 		{@render children()}
 	</div>
