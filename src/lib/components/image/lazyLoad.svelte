3402d4e42ba115f238faf9a20fe6abe20f7ccac9
shamokit <contact@shamokit.com>
2024-10-20 12:06:54 +0900

    📦 update packages

--- src/lib/components/image/lazyLoad.svelte
+++ src/lib/components/image/lazyLoad.svelte
@@ -1,32 +1,48 @@
 <script lang="ts">
-	import IntersectionObserver from 'svelte-intersection-observer';
-	let elementOnce: HTMLElement;
-	let intersectOnce: boolean;
-	export let src: string;
-	export let alt: string;
-	export let width: number;
-	export let height: number;
-	let className: string | null = null;
-	export { className as class };
-	$: isIntersecting = false;
+	let {
+		src,
+		alt,
+		width,
+		height,
+		class: className
+	}: {
+		src: string;
+		alt: string;
+		width: number;
+		height: number;
+		class: string | null;
+	} = $props();
+
+	let image = $state<HTMLElement | null>(null);
+	let isIntersecting = $state<boolean>(false);
+	let intersectOnce = $state<boolean>(false);
+
+	$effect(() => {
+		const intersectionObserver = new IntersectionObserver(
+			(entries) => {
+				if (entries[0].intersectionRatio <= 0) return;
+				if (!intersectOnce) {
+					isIntersecting = true;
+					intersectOnce = true;
+					intersectionObserver.disconnect();
+				}
+			},
+			{
+				threshold: 0
+			}
+		);
+		if (!image) return;
+		intersectionObserver.observe(image);
+	});
 </script>
 
-<IntersectionObserver
-	once
-	element={elementOnce}
-	on:observe={(e) => {
-		isIntersecting = e.detail.isIntersecting;
-	}}
-	bind:intersecting={intersectOnce}
->
-	<img
-		bind:this={elementOnce}
-		src={isIntersecting
-			? src
-			: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAQSURBVHgBAQUA+v8ADVBp/wL6AcYxvWp0AAAAAElFTkSuQmCC'}
-		{alt}
-		{width}
-		{height}
-		class={className ?? null}
-	/>
-</IntersectionObserver>
+<img
+	bind:this={image}
+	src={isIntersecting
+		? src
+		: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAQSURBVHgBAQUA+v8ADVBp/wL6AcYxvWp0AAAAAElFTkSuQmCC'}
+	alt={isIntersecting ? alt : 'loading中…'}
+	{width}
+	{height}
+	class={className ?? null}
+/>
