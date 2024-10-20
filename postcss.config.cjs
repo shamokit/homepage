b401415ee15c74a5228ea61f1272e66a9a7ac991
shamokit <contact@shamokit.com>
2024-10-20 12:26:51 +0900

    🔥 remove unused it

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,12 +1,5 @@
 module.exports = {
 	plugins: {
-		'postcss-nested': {},
-		'postcss-preset-env': {
-			autoprefixer: {
-				flexbox: 'no-2009'
-			},
-			stage: 2
-		},
 		tailwindcss: {},
 		autoprefixer: {}
 	}
